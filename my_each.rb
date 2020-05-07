c08ffa799c1e910b938bf79033e081791768b6f3
Jonathan East <jonathan_east@ymail.com>
2020-05-07 21:50:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
 def my_each(collection) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i += 1
+    end
+    array
+  else
+    return false
+  end
 end
